ff902f158cfc9dbae3e843fae9d0b155f845d198
Akhilesh <akhilesh97733@gmail.com>
2025-08-02 20:44:31 +0530

    Revert "Update layout.js"

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,17 @@
-// app/layout.js
-import { Inter } from 'next/font/google';
-import './globals.css';
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'A issue a day',
   description: 'GitHub Good First Issue Finder',
-};
-
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dark"> {/* Use "dark" for dark mode or "" for light */}
-      <body className={`${inter.className} bg-white text-black dark:bg-gray-950 dark:text-white`}>
-        {children}
-      </body>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
